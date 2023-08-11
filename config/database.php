80af265b965387550707acaf42671664857e025b
dantheman <danogbo0@gmail.com>
2023-08-11 19:49:21 +0100

    recent commit

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'dandev'),
             'username' => env('DB_USERNAME', 'mrdandev'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'root@123'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',

d74d0d914ceef9e18664d1c395660c47480e833e
dantheman <danogbo0@gmail.com>
2023-08-11 20:08:42 +0100

    procfile commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          \App\Models\CitizenDetails::factory(20)->create();
+         \App\Models\Inec::factory(1)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
